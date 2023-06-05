7cf7c48bedbbfc91f6c4ad976f2cf24fdb08861c
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-06-05 15:33:07 +0700

    feat: slicing sign up success

---
+++ src/pages/sign-up-success.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+
+import Wallet from "@/assets/wallet.svg";
+
+export default function SignUpSuccess() {
+  return (
+    <section className="pb-[100px] pt-[150px] md:py-[179px]">
+      <div className="container">
+        <div className="text-center">
+          <Wallet className="h-[300px] w-[315px] max-w-full" />
+        </div>
+        <div className="pb-[150px] pt-[70px] md:pb-[50px]">
+          <h2 className="mb-[10px] text-center text-[32px] font-bold text-dark-blue">
+            Well Done!
+          </h2>
+          <p className="text-center text-lg text-dark-blue">
+            Kamu sudah bisa melakukan top up
+            <br className="hidden sm:block" />
+            dan menjadi pemenang!
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <Button
+            variant="primary"
+            className="font-medium md:mx-auto md:w-[170px]"
+          >
+            Top Up
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
